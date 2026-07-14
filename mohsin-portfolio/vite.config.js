e36c0be67883a5d77aaa35b904cd456b03475450
Fahad-Mehmood-Tech <fahadmehmood8221@gmail.com>
2026-07-14 22:57:52 +0500

    modified:   mohsin-portfolio/package-lock.json 	modified:   mohsin-portfolio/package.json 	modified:   mohsin-portfolio/vite.config.js

--- mohsin-portfolio/vite.config.js
+++ mohsin-portfolio/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mohsin-portfolio/',   // 👈 apne GitHub repo ka exact naam yahan likho
 })
